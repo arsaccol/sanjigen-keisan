0be8e13b05db8a4655a70c36ed67eea41f371934
Alexandre R. Saccol <arsaccol@gmail.com>
2021-06-18 17:29:31 -0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "./matrix.h"
+
+int main()
+{
+    printf("hello there mate\n");
+
+    Matrix<4, 4> mat;
+
+    return 0;
+}
---
+++ matrix.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template <int M, int N>
+struct Matrix
+{
+    float v[M][N];
+};
